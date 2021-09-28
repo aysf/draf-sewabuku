49729193ef64cd8eeed803a1de8d9e82544c3c23
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-28 14:04:58 +0700

    add jwt on user route

--- routes/user_route.go
+++ routes/user_route.go
@@ -2,13 +2,18 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"os"
 	"sewabuku/controllers/user"
 )
 
 func UserPath(e *echo.Echo, userController *user.Controller) {
+	jwtAuth := e.Group("")
+	jwtAuth.Use(middleware.JWT([]byte(os.Getenv("SECRET_KEY"))))
+
 	e.POST("/users/register", userController.RegisterUserController)
 
 	e.POST("/users/login", userController.LoginUserController)
 
-	e.GET("/users", userController.GetUserProfileController)
+	jwtAuth.GET("/users/profile", userController.GetUserProfileController)
 }
