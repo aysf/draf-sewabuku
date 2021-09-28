1b91d8fefb2604e3353a515ed637a8d0eac40db6
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-28 20:17:31 +0700

    add comment on method

--- database/user_repo.go
+++ database/user_repo.go
@@ -19,10 +19,12 @@ type (
 	}
 )
 
+// NewUserModel method repo init
 func NewUserModel(db *gorm.DB) *GormUserModel {
 	return &GormUserModel{db: db}
 }
 
+// Register method to add new user
 func (g *GormUserModel) Register(user models.User) (models.User, error) {
 	if err := g.db.Create(&user).Error; err != nil {
 		return user, err
@@ -31,6 +33,7 @@ func (g *GormUserModel) Register(user models.User) (models.User, error) {
 	return user, nil
 }
 
+// Login method to user log in
 func (g *GormUserModel) Login(email, password string) (models.User, error) {
 	var user models.User
 	var err error
@@ -52,6 +55,7 @@ func (g *GormUserModel) Login(email, password string) (models.User, error) {
 	return user, nil
 }
 
+// GetProfile method to get user profile
 func (g *GormUserModel) GetProfile(userId int) (models.User, error) {
 	var user models.User
 
@@ -62,6 +66,7 @@ func (g *GormUserModel) GetProfile(userId int) (models.User, error) {
 	return user, nil
 }
 
+// UpdatePassword method to edit user password
 func (g *GormUserModel) UpdatePassword(newPass models.User, userId int) (models.User, error) {
 	var user models.User
 	var err error
