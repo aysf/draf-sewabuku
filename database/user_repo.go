5875caa9fcb807f34ffbe25c73d581c2bcd168de
Tegar Aditia Pratama <tegara946@gmail.com>
2021-09-30 22:46:29 +0700

    fixing encryption new password

--- database/user_repo.go
+++ database/user_repo.go
@@ -19,6 +19,7 @@ type (
 		Register(user models.User) (models.User, error)
 		Login(email, password string) (models.User, error)
 		GetProfile(userId int) (models.User, error)
+		EditProfile(userId int) (models.User, error)
 		UpdatePassword(newPass models.User, userId int) (models.User, error)
 	}
 )
@@ -86,14 +87,31 @@ func (g *GormUserModel) GetProfile(userId int) (models.User, error) {
 	return user, nil
 }
 
+// EditProfile is  method to get user profile
+func (g *GormUserModel) EditProfile(userId int) (models.User, error) {
+	var user models.User
+
+	if err := g.db.Save(&user).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
 // UpdatePassword is method to edit user password
 func (g *GormUserModel) UpdatePassword(newPass models.User, userId int) (models.User, error) {
 	var user models.User
 	var err error
 
-	g.db.First(&user, userId)
+	if err = g.db.First(&user, userId).Error; err != nil {
+		return user, err
+	}
 
-	user.Password = newPass.Password
+	bcryptCost, _ := strconv.Atoi(os.Getenv("BCRYPT_COST"))
+
+	passwordEncrypted, _ := bcrypt.GenerateFromPassword([]byte(newPass.Password), bcryptCost)
+
+	user.Password = string(passwordEncrypted)
 
 	if err = g.db.Save(&user).Error; err != nil {
 		return user, err
