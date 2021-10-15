74c5d73d1cc81afe7da6d22452d2396851a8e6f2
Tegar Aditia Pratama <tegara946@gmail.com>
2021-10-15 07:35:51 +0700

    fixing unit test on user

--- controllers/user/user_controller_test.go
+++ controllers/user/user_controller_test.go
@@ -59,7 +59,7 @@ func TestController_RegisterUserController(t *testing.T) {
 		},
 		{
 			name:            "test3",
-			reqBody:         map[string]string{"name": "agito", "email": "", "password": "", "address": ""},
+			reqBody:         map[string]string{"name": "ryuuki", "organization_name": "Kamen Rider", "email": "ryuki@rider.jp", "password": "ryukimaru", "address": "japan"},
 			expectCode:      http.StatusInternalServerError,
 			responseStatus:  "error",
 			responseMessage: "Register Failed",
@@ -68,16 +68,6 @@ func TestController_RegisterUserController(t *testing.T) {
 
 	// Initialize database connection
 	db := config.DBConnectTest()
-
-	// Drop and create new table
-	//db.Migrator().DropTable(
-	//	&models.Account{},
-	//	&models.User{},
-	//)
-	//db.AutoMigrate(
-	//	&models.Account{},
-	//	&models.User{},
-	//)
 
 	// Initialize server
 	e := echo.New()
@@ -147,10 +137,6 @@ func TestController_LoginUserController(t *testing.T) {
 
 	// Initialize database connection
 	db := config.DBConnectTest()
-
-	// Drop and create new table
-	//db.Migrator().DropTable(&models.User{})
-	//db.AutoMigrate(&models.User{})
 
 	// Prepare dummy data
 	newUser := models.User{
@@ -281,22 +267,6 @@ func TestController_UpdateUserProfileController(t *testing.T) {
 	// Initialize database connection
 	db := config.DBConnectTest()
 
-	// Drop and create new table
-	//db.Migrator().DropTable(&models.User{})
-	//db.AutoMigrate(&models.User{})
-
-	// Prepare dummy data
-	newUser := models.User{
-		Name:     "Test Login",
-		Email:    "test1@test.com",
-		Password: "1234pass",
-	}
-	registerModel := database.NewUserModel(db)
-	_, err := registerModel.Register(newUser)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	// Create token
 	token, _ := middlewares.CreateToken(1)
 
@@ -320,7 +290,7 @@ func TestController_UpdateUserProfileController(t *testing.T) {
 		}
 		res := httptest.NewRecorder()
 		ctx := e.NewContext(req, res)
-		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.GetUserProfileController)(ctx)) {
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.UpdateUserProfileController)(ctx)) {
 			resBody := res.Body.String()
 
 			var response userResponse
@@ -361,22 +331,6 @@ func TestController_UpdatePasswordController(t *testing.T) {
 	// Initialize database connection
 	db := config.DBConnectTest()
 
-	// Drop and create new table
-	//db.Migrator().DropTable(&models.User{})
-	//db.AutoMigrate(&models.User{})
-
-	// Prepare dummy data
-	newUser := models.User{
-		Name:     "Test Login",
-		Email:    "test1@test.com",
-		Password: "1234pass",
-	}
-	registerModel := database.NewUserModel(db)
-	_, err := registerModel.Register(newUser)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	// Create token
 	token, _ := middlewares.CreateToken(1)
 
@@ -400,7 +354,7 @@ func TestController_UpdatePasswordController(t *testing.T) {
 		}
 		res := httptest.NewRecorder()
 		ctx := e.NewContext(req, res)
-		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.GetUserProfileController)(ctx)) {
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.UpdatePasswordController)(ctx)) {
 			resBody := res.Body.String()
 
 			var response userResponse
@@ -413,3 +367,334 @@ func TestController_UpdatePasswordController(t *testing.T) {
 	}
 }
 
+func TestController_LogoutUserController(t *testing.T) {
+	//Initialize test cases
+	var testCases = []struct {
+		name            string
+		expectCode      int
+		responseStatus  string
+		responseMessage string
+	}{
+		{
+			name:            "test1",
+			expectCode:      http.StatusOK,
+			responseStatus:  "success",
+			responseMessage: "Logout Success",
+		},
+		{
+			name:            "test2",
+			expectCode:      http.StatusBadRequest,
+			responseStatus:  "fail",
+			responseMessage: "Logout Failed",
+		},
+	}
+
+	// Initialize database connection
+	db := config.DBConnectTest()
+
+	// Create token
+	token, _ := middlewares.CreateToken(1)
+
+	// Initialize server
+	e := echo.New()
+
+	// Initialize user model
+	modelUser := database.NewUserModel(db)
+
+	// Initialize user controller
+	controllerUser := NewController(modelUser)
+
+	// Process all test cases
+	for _, testCase := range testCases {
+		req := httptest.NewRequest(echo.GET, "/users/logout", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		if testCase.name == "test2" {
+			db.Migrator().DropTable(&models.User{})
+		}
+		res := httptest.NewRecorder()
+		ctx := e.NewContext(req, res)
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.LogoutUserController)(ctx)) {
+			resBody := res.Body.String()
+
+			var response userResponse
+			json.Unmarshal([]byte(resBody), &response)
+
+			assert.Equal(t, testCase.expectCode, res.Code)
+			assert.Equal(t, testCase.responseStatus, response.Status)
+			assert.Equal(t, testCase.responseMessage, response.Message)
+		}
+	}
+}
+
+func TestController_GetBorrowedController(t *testing.T) {
+	//Initialize test cases
+	var testCases = []struct {
+		name            string
+		expectCode      int
+		responseStatus  string
+		responseMessage string
+	}{
+		{
+			name:            "test1",
+			expectCode:      http.StatusOK,
+			responseStatus:  "success",
+			responseMessage: "Success Get Borrowed Book",
+		},
+		{
+			name:            "test2",
+			expectCode:      http.StatusBadRequest,
+			responseStatus:  "fail",
+			responseMessage: "Fail to Get Borrowed Book",
+		},
+	}
+
+	// Initialize database connection
+	db := config.DBConnectTest()
+
+	// Create token
+	token, _ := middlewares.CreateToken(1)
+
+	// Initialize server
+	e := echo.New()
+
+	// Initialize user model
+	modelUser := database.NewUserModel(db)
+
+	// Initialize user controller
+	controllerUser := NewController(modelUser)
+
+	// Process all test cases
+	for _, testCase := range testCases {
+		req := httptest.NewRequest(echo.GET, "/users/cart", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		if testCase.name == "test2" {
+			db.Migrator().DropTable(&models.User{})
+		}
+		res := httptest.NewRecorder()
+		ctx := e.NewContext(req, res)
+		ctx.QueryParam("complete")
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.GetBorrowedController)(ctx)) {
+			resBody := res.Body.String()
+
+			var response userResponse
+			json.Unmarshal([]byte(resBody), &response)
+
+			assert.Equal(t, testCase.expectCode, res.Code)
+			assert.Equal(t, testCase.responseStatus, response.Status)
+			assert.Equal(t, testCase.responseMessage, response.Message)
+		}
+	}
+}
+
+func TestController_GetLentController(t *testing.T) {
+	//Initialize test cases
+	var testCases = []struct {
+		name            string
+		expectCode      int
+		responseStatus  string
+		responseMessage string
+	}{
+		{
+			name:            "test1",
+			expectCode:      http.StatusOK,
+			responseStatus:  "success",
+			responseMessage: "Success Get Lent Book",
+		},
+		{
+			name:            "test2",
+			expectCode:      http.StatusBadRequest,
+			responseStatus:  "fail",
+			responseMessage: "Fail to Get Lent Book",
+		},
+	}
+
+	// Initialize database connection
+	db := config.DBConnectTest()
+
+	// Create token
+	token, _ := middlewares.CreateToken(1)
+
+	// Initialize server
+	e := echo.New()
+
+	// Initialize user model
+	modelUser := database.NewUserModel(db)
+
+	// Initialize user controller
+	controllerUser := NewController(modelUser)
+
+	// Process all test cases
+	for _, testCase := range testCases {
+		req := httptest.NewRequest(echo.GET, "/users/books", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		if testCase.name == "test2" {
+			db.Migrator().DropTable(&models.User{})
+		}
+		res := httptest.NewRecorder()
+		ctx := e.NewContext(req, res)
+		ctx.QueryParam("complete")
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.GetLentController)(ctx)) {
+			resBody := res.Body.String()
+
+			var response userResponse
+			json.Unmarshal([]byte(resBody), &response)
+
+			assert.Equal(t, testCase.expectCode, res.Code)
+			assert.Equal(t, testCase.responseStatus, response.Status)
+			assert.Equal(t, testCase.responseMessage, response.Message)
+		}
+	}
+}
+
+func TestController_InsertRatingBookController(t *testing.T) {
+	//Initialize test cases
+	var testCases = []struct {
+		name            string
+		reqBody         map[string]string
+		expectCode      int
+		responseStatus  string
+		responseMessage string
+	}{
+		{
+			name:            "test1",
+			reqBody:         map[string]string{"rate_book": "5", "desc_rate_book": "Bukunya bagus bingit"},
+			expectCode:      http.StatusOK,
+			responseStatus:  "success",
+			responseMessage: "Success Give Book Rating",
+		},
+		{
+			name:            "test2",
+			reqBody:         map[string]string{"rate_book": "2", "desc_rate_book": ""},
+			expectCode:      http.StatusBadRequest,
+			responseStatus:  "fail",
+			responseMessage: "Fail to Give Book Rating",
+		},
+	}
+
+	// Initialize database connection
+	db := config.DBConnectTest()
+
+	// Initialize server
+	e := echo.New()
+
+	// Add validator module
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	// Initialize user model
+	modelUser := database.NewUserModel(db)
+
+	// Initialize user controller
+	controllerUser := NewController(modelUser)
+
+	// Create token
+	token, _ := middlewares.CreateToken(1)
+
+	// Process all test cases
+	for _, testCase := range testCases {
+		if testCase.name == "test2" {
+			db.Migrator().DropTable(
+				&models.Rating{},
+			)
+		}
+		body, _ := json.Marshal(testCase.reqBody)
+		fmt.Println(testCase.reqBody)
+		req := httptest.NewRequest(echo.POST, "/users/book-rating", bytes.NewBuffer(body))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		res := httptest.NewRecorder()
+		ctx := e.NewContext(req, res)
+		ctx.SetPath("/:id")
+		ctx.SetParamNames("id")
+		ctx.SetParamValues("1")
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.InsertRatingBookController)(ctx)) {
+			resBody := res.Body.String()
+
+			fmt.Println(resBody)
+
+			var response userResponse
+			json.Unmarshal([]byte(resBody), &response)
+
+			assert.Equal(t, testCase.expectCode, res.Code)
+			assert.Equal(t, testCase.responseStatus, response.Status)
+			assert.Equal(t, testCase.responseMessage, response.Message)
+		}
+	}
+}
+
+func TestController_InsertRatingBorrowerController(t *testing.T) {
+	//Initialize test cases
+	var testCases = []struct {
+		name            string
+		reqBody         map[string]string
+		expectCode      int
+		responseStatus  string
+		responseMessage string
+	}{
+		{
+			name:            "test1",
+			reqBody:         map[string]string{"rate_borrower": "5", "desc_rate_borrower": "Bukunya bagus bingit"},
+			expectCode:      http.StatusOK,
+			responseStatus:  "success",
+			responseMessage: "Success Give Book Rating",
+		},
+		{
+			name:            "test2",
+			reqBody:         map[string]string{"rate_borrower": "2", "desc_rate_borrower": ""},
+			expectCode:      http.StatusBadRequest,
+			responseStatus:  "fail",
+			responseMessage: "Fail to Give Book Rating",
+		},
+	}
+
+	// Initialize database connection
+	db := config.DBConnectTest()
+
+	// Initialize server
+	e := echo.New()
+
+	// Add validator module
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	// Initialize user model
+	modelUser := database.NewUserModel(db)
+
+	// Initialize user controller
+	controllerUser := NewController(modelUser)
+
+	// Create token
+	token, _ := middlewares.CreateToken(1)
+
+	// Process all test cases
+	for _, testCase := range testCases {
+		if testCase.name == "test2" {
+			db.Migrator().DropTable(
+				&models.Rating{},
+			)
+		}
+		body, _ := json.Marshal(testCase.reqBody)
+		fmt.Println(testCase.reqBody)
+		req := httptest.NewRequest(echo.POST, "/users/borrower-rating", bytes.NewBuffer(body))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		res := httptest.NewRecorder()
+		ctx := e.NewContext(req, res)
+		ctx.SetPath("/:id")
+		ctx.SetParamNames("id")
+		ctx.SetParamValues("1")
+		if assert.NoError(t, m.JWT([]byte(os.Getenv("SECRET_KEY")))(controllerUser.InsertRatingBorrowerController)(ctx)) {
+			resBody := res.Body.String()
+
+			fmt.Println(resBody)
+
+			var response userResponse
+			json.Unmarshal([]byte(resBody), &response)
+
+			assert.Equal(t, testCase.expectCode, res.Code)
+			assert.Equal(t, testCase.responseStatus, response.Status)
+			assert.Equal(t, testCase.responseMessage, response.Message)
+		}
+	}
+}
